66f820481294364af6060f590ac6b8571e64dea2
Ivan Cherniy <paalomnik@gmail.com>
2018-02-03 22:49:55 +0200

    fix

--- rig/2setup_cards.sh
+++ rig/2setup_cards.sh
@@ -7,9 +7,7 @@ then
 fi
 
 systemctl stop lightdm.service
-nvidia-xconfig -s -a --allow-empty-initial-configuration \
---cool-bits=12 --registry-dwords="PerfLevelSrc=0x2222" \
---no-sli --connected-monitor="DFP-0" -o /etc/X11/xorg.conf
+nvidia-xconfig -s -a --allow-empty-initial-configuration --cool-bits=12 --registry-dwords="PerfLevelSrc=0x2222" --no-sli --connected-monitor="DFP-0" -o /etc/X11/xorg.conf
 
 sed -i '/Driver/a Option "Interactive" "False"' /etc/X11/xorg.conf
 sed -i '/    Driver         "nvidia"/ a \ \ \ \ Option         "Coolbits" "28"' /etc/X11/xorg.conf
